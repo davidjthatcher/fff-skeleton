53d9ed5d63cd8bbef39b27ab2e59ce5a537cb73b
davidjthatcher <davidjohnthatcher@gmail.com>
2016-04-26 07:02:12 -0400

    Add account for Luke and mom.

--- app/db/user.sql
+++ app/db/user.sql
@@ -9,4 +9,10 @@ PRIMARY KEY (id)
 );
 
 INSERT INTO user (id, username, password)
-VALUES (1,'john','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua');
+VALUES (1,'david','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua');
+
+INSERT INTO user (id, username, password)
+VALUES (2,'luke','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua');
+
+INSERT INTO user (id, username, password)
+VALUES (3,'judy','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua');
