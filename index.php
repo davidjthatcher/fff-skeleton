3e4052e8db51934df82d45ad6f67c4deafb40789
davidjthatcher <davidjohnthatcher@gmail.com>
2016-04-26 10:32:16 -0400

    Udpate for fff at /var/usr/

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once("vendor/autoload.php");
+$f3 = require("/var/lib/fatfree-master/lib/base.php");
+
+$f3->set('AUTOLOAD','app/models; app/controllers');
 
 $f3 = Base::instance();
 
