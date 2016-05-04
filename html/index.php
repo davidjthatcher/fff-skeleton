112e2eb2fc46ec2d18d417311a9c1eb21bdd35da
davidjthatcher <davidjohnthatcher@gmail.com>
2016-05-04 17:06:29 -0400

    Move non web files out of document root (config.ini, routes.ini, app/models, app/controllers)

--- html/index.php
+++ html/index.php
@@ -2,12 +2,10 @@
 
 $f3 = require("/var/lib/fatfree-master/lib/base.php");
 
-$f3->set('AUTOLOAD','app/models; app/controllers');
-
 $f3 = Base::instance();
 
-$f3->config('config.ini');
-$f3->config('routes.ini');
+$f3->config('../local/config.ini');
+$f3->config('../local/routes.ini');
 
 new Session();
 
