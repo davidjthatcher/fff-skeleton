036381e7bf19cb1f748aea2c6b8f74cf633f17ad
davidjthatcher <davidjohnthatcher@gmail.com>
2016-06-14 16:57:13 -0400

    Add order status, order start user preference settings.

--- local/app/db/user.sql
+++ local/app/db/user.sql
@@ -5,14 +5,23 @@ CREATE TABLE user
 id int(11) NOT NULL AUTO_INCREMENT,
 username varchar(45) NOT NULL,
 password varchar(95) NOT NULL,
+order_status varchar(15) NOT NULL,
+order_start_date date NOT NULL,
 PRIMARY KEY (id)
 );
 
-INSERT INTO user (id, username, password)
-VALUES (1,'david','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua');
+INSERT INTO user (id, username, password, order_status, order_start_date)
+VALUES (1,'david','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua',
+    'processing', "2016-01-01");
 
-INSERT INTO user (id, username, password)
-VALUES (2,'luke','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua');
+INSERT INTO user (id, username, password, order_status, order_start_date)
+VALUES (2,'luke','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua',
+    'processing', "2016-01-01");
 
-INSERT INTO user (id, username, password)
-VALUES (3,'judy','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua');
+INSERT INTO user (id, username, password, order_status, order_start_date)
+VALUES (3,'judy','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua',
+    'processing', "2016-01-01");
+
+INSERT INTO user (id, username, password, order_status, order_start_date)
+VALUES (4,'sean','$2y$10$QdRxyYlRFqG0/ltueW1Tj.9RZjN5w4pq/8mZSCUpvzvT58sHQylua',
+    'processing', "2016-01-01");
