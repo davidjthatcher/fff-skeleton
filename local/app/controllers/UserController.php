1ff29964999dc813c3118762a34ea6f39999f858
davidjthatcher <davidjohnthatcher@gmail.com>
2016-08-29 11:22:47 -0400

    Implement user edit and change password. Implement user logout nav option and controller.

--- local/app/controllers/UserController.php
+++ local/app/controllers/UserController.php
@@ -82,4 +82,107 @@ class UserController extends Controller
             $this->f3->reroute('/login');
         }
     }
+    /**
+     * List Users who have access to system.
+     */
+    function userList()
+    {
+        $user = new User($this->db);
+        $users = $user->all();
+
+        $this->f3->set('users', $users );
+        $this->f3->set('view', 'userList.htm');
+
+        $template=new Template;
+        echo $template->render('layout.htm');
+    }
+    /**
+     * Edit User information
+     */
+    function userEdit()
+    {
+        $query = $this->f3->get('QUERY');
+        $qvars = array();
+        parse_str($query, $qvars);
+
+        $user = new User($this->db);
+        $user->getById($qvars['id']);
+
+        echo $user->username;
+        echo json_encode($myUser);
+
+        $this->f3->set( 'user', $user );
+        $this->f3->set('view', 'userEdit.htm');
+
+        $template=new Template;
+        echo $template->render('layout.htm');
+    }
+    /**
+     * List Users who have access to system.
+     * If passsword has been changed, create new hash
+     */
+    function userUpdate()
+    {
+        $id = $this->f3->get('POST.id');
+
+        $user = new User($this->db);
+        $user->edit($id);
+
+        $this->userList();
+    }
+
+    /**
+     * Edit User Password
+     */
+    function userEditPassword()
+    {
+        $username = $this->f3->get('SESSION.user');
+        $user = new User($this->db);
+        $user->getByName($username);
+
+        $this->f3->set('user', $user );
+        $this->f3->set('view', 'userEditPassword.htm');
+
+        $template=new Template;
+        echo $template->render('layout.htm');
+    }
+    /**
+     * Update Password
+     * If passsword has been changed, create new hash
+     */
+    function userUpdatePassword()
+    {
+        $id = $this->f3->get('POST.id');
+        $user = new User($this->db);
+        $user->getById($id);
+
+        /* Only update password for current user */
+        if( $id == $user->id ){
+
+            /* Password updates require new hash */
+            $password1 = $this->f3->get('POST.password1');
+            $password2 = $this->f3->get('POST.password2');
+
+            /* Ensure both new passwords match */
+            if($password1 == $password2){
+
+                if( $password1 == $user->password ) {
+                    /* Nothing to do if no password change */
+                } else {
+                    $secret = password_hash($password1, PASSWORD_BCRYPT);
+                    $this->f3->set('POST.password', $secret);
+                    $user->edit($id);
+                }
+            }
+        }
+
+        $this->f3->reroute('/bookingSummary');
+    }
+
+    function userLogout()
+    {
+        /* Log out of system by reseting SESSION user */
+        $this->f3->set('SESSION.user', null);
+        $this->f3->reroute('/login');
+    }
 }
--- local/app/models/User.php
+++ local/app/models/User.php
@@ -3,41 +3,41 @@
  * User model
  *
  * PHP version 5
- * 
+ *
  * @category PHP
  * @package  Fat-Free-PHP-Bootstrap-Site
  * @author   Mark Takacs <takacsmark@takacsmark.com>
- * @license  MIT 
+ * @license  MIT
  * @link     takacsmark.com
  */
- 
+
  /**
  * User model class
- * 
+ *
  * @category PHP
  * @package  Fat-Free-PHP-Bootstrap-Site
  * @author   Mark Takacs <takacsmark@takacsmark.com>
- * @license  MIT 
+ * @license  MIT
  * @link     takacsmark.com
  */
 class User extends DB\SQL\Mapper
 {
     /**
     * Constructor, maps user table fields to php object
-    * 
+    *
     * @param DB\SQL $db Database connection
     */
-    public function __construct(DB\SQL $db) 
+    public function __construct(DB\SQL $db)
     {
         parent::__construct($db, 'user');
     }
 
     /**
     * Fetch all records
-    * 
+    *
     * @return array
-    */        
-    public function all() 
+    */
+    public function all()
     {
         $this->load();
         return $this->query;
@@ -45,12 +45,12 @@ class User extends DB\SQL\Mapper
 
     /**
     * Fetch one record by id records
-    * 
+    *
     * @param int $id User id
     *
-    * @return array
-    */  
-    public function getById($id) 
+    * @return array (TBD not working for me?)
+    */
+    public function getById($id)
     {
         $this->load(array('id=?',$id));
         return $this->query;
@@ -58,12 +58,12 @@ class User extends DB\SQL\Mapper
 
     /**
     * Fetch one record by name records
-    * 
+    *
     * @param string $name User name
     *
     * @return array
-    */  
-    public function getByName($name) 
+    */
+    public function getByName($name)
     {
         $this->load(array('username=?', $name));
     }
@@ -72,10 +72,10 @@ class User extends DB\SQL\Mapper
     * Add a User record
     * there are no paramaters, becuase record data is copied from $_POST
     * it's one of the great features of f3
-    * 
+    *
     * @return void
-    */    
-    public function add() 
+    */
+    public function add()
     {
         $this->copyFrom('POST');
         $this->save();
@@ -86,24 +86,24 @@ class User extends DB\SQL\Mapper
     * Edit a specific record
     *
     * @param int $id User id
-    * 
+    *
     * @return void
-    */        
-    public function edit($id) 
+    */
+    public function edit($id)
     {
         $this->load(array('id=?',$id));
         $this->copyFrom('POST');
         $this->update();
     }
-    
+
     /**
     * Delete a record
     *
     * @param int $id User id
-    *      
+    *
     * @return void
-    */    
-    public function delete($id) 
+    */
+    public function delete($id)
     {
         $this->load(array('id=?',$id));
         $this->erase();
